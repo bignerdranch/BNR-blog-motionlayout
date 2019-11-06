cbd8b77754ae60c79ddc9e9062eef97c1b4f86a5
Dan Nesfeder <dnesfeder@etsy.com>
2019-11-06 12:27:49 -0600

    Initial commit

---
+++ app/src/main/java/com/bignerdranch/motionapplication/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.bignerdranch.motionapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(map: GoogleMap) {
+        // No-op
+    }
+}
