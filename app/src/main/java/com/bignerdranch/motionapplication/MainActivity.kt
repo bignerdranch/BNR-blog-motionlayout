fb0e5b0f1015b28c328e63a57364928711002157
Dan Nesfeder <dnesfeder@etsy.com>
2019-11-29 17:58:51 -0500

    Set map position to ATX

--- app/src/main/java/com/bignerdranch/motionapplication/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/motionapplication/MainActivity.kt
@@ -2,9 +2,12 @@ package com.bignerdranch.motionapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
@@ -17,6 +20,8 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     }
 
     override fun onMapReady(map: GoogleMap) {
-        // No-op
+        val atxLatLng = LatLng(30.266666, -97.733330)
+        val atxCameraPosition = CameraPosition.fromLatLngZoom(atxLatLng, 12f)
+        map.animateCamera(CameraUpdateFactory.newCameraPosition(atxCameraPosition))
     }
 }
